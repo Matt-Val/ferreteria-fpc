4de7cf0eacd48a93356a9e77b049f8a40470878e
Matt <mat.ramosv@duocuc.cl>
2026-03-05 23:35:41 -0300

    solucion de problemas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,15 @@ const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <>
+        <div className="header-wrapper">
             {/* Top info bar */}
             <div className="top-bar">
                 <div className="container top-bar-inner">
                     <div className="top-bar-contacts">
+                        <a href="tel:+56948542010">
+                            <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.55 3.4 2 2 0 0 1 3.52 1h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.5a16 16 0 0 0 6 6l.86-.86a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
+                            +56 9 4854 2010
+                        </a>
                         <a href="tel:+56954274397">
                             <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.55 3.4 2 2 0 0 1 3.52 1h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.5a16 16 0 0 0 6 6l.86-.86a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
                             +56 9 5427 4397
@@ -59,7 +63,7 @@ const Header = () => {
                     </button>
                 </div>
             </header>
-        </>
+        </div>
     );
 };
 
