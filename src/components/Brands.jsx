50f9430988a2fc9aea173ad44cd05f2ee7695004
Matt-Val <mat.ramosv@duocuc.cl>
2026-03-06 13:58:19 -0300

    arreglo y añadido de logos nuevos

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -4,14 +4,15 @@ import '../styles/Brands.css';
 const brands = [
   { id: 1, name: 'HELA', logo: '/brands/logo01.png' },
   { id: 2, name: '3M', logo: '/brands/logo02.png' },
-  { id: 3, name: 'Finder', logo: '/brands/logo03.png' },
-  { id: 4, name: 'Gladiator', logo: '/brands/logo04.png' },
+  { id: 3, name: 'DeWalt', logo: '/brands/logo03.jpg' },
+  { id: 4, name: 'Lincoln', logo: '/brands/logo04.jpeg' },
   { id: 5, name: 'Harden', logo: '/brands/logo05.png' },
   { id: 6, name: 'Indura', logo: '/brands/logo06.png' },
   { id: 7, name: 'Krafter', logo: '/brands/logo07.png' },
   { id: 8, name: 'Makita', logo: '/brands/logo08.png' },
   { id: 9, name: 'Total', logo: '/brands/logo09.png' },
   { id: 10, name: 'Würth', logo: '/brands/logo10.png' },
+  { id: 11, name: 'Linde', logo: '/brands/logo11.jfif' },
 ];
 
 const Brands = () => (
